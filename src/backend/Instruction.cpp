d8db1d3b15d2690c8cf9192b419704f3cd88e4fe
KiwiGrenade <kubajaskow@gmail.com>
2024-09-26 13:57:19 +0200

    Move constructor definitions to .cpp files

--- src/backend/Instruction.cpp
+++ src/backend/Instruction.cpp
@@ -1,3 +1,18 @@
 #include "Instruction.hpp"
 
+Instruction::Instruction(const QJsonObject& obj)
+        : info_(Info(obj)) {
+}
+
+    // should there be reference to a function or just function object?
+Instruction::Instruction(const QJsonObject& obj, std::function<bool()> func)
+        : info_(Info(obj))
+        , instr_(func) {
+}
+
+Instruction::Info::Info(const QJsonObject& obj)
+    : bytes_(Utils::getBytesFromInstrJsonObject(obj))
+    , cycles_(Utils::getCyclesFromInstrJsonObject(obj))
+    , flags_(Utils::getFlagsFromInstrJsonObject(obj)) {
+}
 
