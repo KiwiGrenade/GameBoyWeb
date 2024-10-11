e02c065d2208af8ac0961294fb272d952531b591
jaskow <jakub.jaskow@nokia.com>
2024-10-11 17:35:35 +0200

    Implement all opcodes - have to add tests

--- src/backend/ProceduresUnprefixed.cpp
+++ src/backend/ProceduresUnprefixed.cpp
@@ -1,7 +1,4 @@
 #include "CPU.hpp"
-#include "utils.hpp"
-
-void notImplemented() { Utils::warning("Opcode not implemented!"); }
 
 ProcArray CPU::getUnprefProcArray() {
     return {
@@ -14,7 +11,7 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0x06*/ { ld(B_, fetch8(PC_+1)); },
         [this] /*0x07*/ { rlca(); },
         [this] /*0x08*/ { ldd16(fetch16(PC_+1), SP_); },
-        [this] /*0x09*/ { notImplemented(); },
+        [this] /*0x09*/ { add16(BC_); },
         [this] /*0x0A*/ { ld(A_, fetch8(BC_)); },
         [this] /*0x0B*/ { dec(BC_); },
         [this] /*0x0C*/ { inc(C_); },
@@ -30,7 +27,7 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0x16*/ { ld(D_, fetch8(PC_+1)); },
         [this] /*0x17*/ { rla(); },
         [this] /*0x18*/ { jr(true, static_cast<int8_t>(fetch8(PC_+1))); },
-        [this] /*0x19*/ { notImplemented(); },
+        [this] /*0x19*/ { add16(DE_); },
         [this] /*0x1A*/ { ld(A_, fetch8(DE_)); },
         [this] /*0x1B*/ { dec(DE_); },
         [this] /*0x1C*/ { inc(E_); },
@@ -46,7 +43,7 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0x26*/ { ld(H_, fetch8(PC_+1)); },
         [this] /*0x27*/ { daa(); },
         [this] /*0x28*/ { jr(FlagZ_, static_cast<int8_t>(fetch8(PC_+1))); },
-        [this] /*0x29*/ { notImplemented(); },
+        [this] /*0x29*/ { add16(HL_); },
         [this] /*0x2A*/ { ld(A_, fetch8(HL_++)); },
         [this] /*0x2B*/ { dec(HL_); },
         [this] /*0x2C*/ { inc(L_); },
@@ -62,7 +59,7 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0x36*/ { ldd(HL_, fetch8(PC_+1)); },
         [this] /*0x37*/ { }, // handleFlags does the job
         [this] /*0x38*/ { jr(FlagC_, static_cast<int8_t>(fetch8(PC_+1))); },
-        [this] /*0x39*/ { notImplemented(); },
+        [this] /*0x39*/ { add16(SP_); },
         [this] /*0x3A*/ { ld(A_, fetch8(HL_--)); },
         [this] /*0x3B*/ { dec(SP_); },
         [this] /*0x3C*/ { inc(A_); },
@@ -133,93 +130,93 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0x7D*/ { ld(A_, L_); },
         [this] /*0x7E*/ { ld(A_, fetch8(HL_)); },
         [this] /*0x7F*/ { ld(A_, A_); },
-        [this] /*0x80*/ { notImplemented(); },
-        [this] /*0x81*/ { notImplemented(); },
-        [this] /*0x82*/ { notImplemented(); },
-        [this] /*0x83*/ { notImplemented(); },
-        [this] /*0x84*/ { notImplemented(); },
-        [this] /*0x85*/ { notImplemented(); },
-        [this] /*0x86*/ { notImplemented(); },
-        [this] /*0x87*/ { notImplemented(); },
-        [this] /*0x88*/ { notImplemented(); },
-        [this] /*0x89*/ { notImplemented(); },
-        [this] /*0x8A*/ { notImplemented(); },
-        [this] /*0x8B*/ { notImplemented(); },
-        [this] /*0x8C*/ { notImplemented(); },
-        [this] /*0x8D*/ { notImplemented(); },
-        [this] /*0x8E*/ { notImplemented(); },
-        [this] /*0x8F*/ { notImplemented(); },
-        [this] /*0x90*/ { notImplemented(); },
-        [this] /*0x91*/ { notImplemented(); },
-        [this] /*0x92*/ { notImplemented(); },
-        [this] /*0x93*/ { notImplemented(); },
-        [this] /*0x94*/ { notImplemented(); },
-        [this] /*0x95*/ { notImplemented(); },
-        [this] /*0x96*/ { notImplemented(); },
-        [this] /*0x97*/ { notImplemented(); },
-        [this] /*0x98*/ { notImplemented(); },
-        [this] /*0x99*/ { notImplemented(); },
-        [this] /*0x9A*/ { notImplemented(); },
-        [this] /*0x9B*/ { notImplemented(); },
-        [this] /*0x9C*/ { notImplemented(); },
-        [this] /*0x9D*/ { notImplemented(); },
-        [this] /*0x9E*/ { notImplemented(); },
-        [this] /*0x9F*/ { notImplemented(); },
-        [this] /*0xA0*/ { notImplemented(); },
-        [this] /*0xA1*/ { notImplemented(); },
-        [this] /*0xA2*/ { notImplemented(); },
-        [this] /*0xA3*/ { notImplemented(); },
-        [this] /*0xA4*/ { notImplemented(); },
-        [this] /*0xA5*/ { notImplemented(); },
-        [this] /*0xA6*/ { notImplemented(); },
-        [this] /*0xA7*/ { notImplemented(); },
-        [this] /*0xA8*/ { notImplemented(); },
-        [this] /*0xA9*/ { notImplemented(); },
-        [this] /*0xAA*/ { notImplemented(); },
-        [this] /*0xAB*/ { notImplemented(); },
-        [this] /*0xAC*/ { notImplemented(); },
-        [this] /*0xAD*/ { notImplemented(); },
-        [this] /*0xAE*/ { notImplemented(); },
-        [this] /*0xAF*/ { notImplemented(); },
-        [this] /*0xB0*/ { notImplemented(); },
-        [this] /*0xB1*/ { notImplemented(); },
-        [this] /*0xB2*/ { notImplemented(); },
-        [this] /*0xB3*/ { notImplemented(); },
-        [this] /*0xB4*/ { notImplemented(); },
-        [this] /*0xB5*/ { notImplemented(); },
-        [this] /*0xB6*/ { notImplemented(); },
-        [this] /*0xB7*/ { notImplemented(); },
-        [this] /*0xB8*/ { notImplemented(); },
-        [this] /*0xB9*/ { notImplemented(); },
-        [this] /*0xBA*/ { notImplemented(); },
-        [this] /*0xBB*/ { notImplemented(); },
-        [this] /*0xBC*/ { notImplemented(); },
-        [this] /*0xBD*/ { notImplemented(); },
-        [this] /*0xBE*/ { notImplemented(); },
-        [this] /*0xBF*/ { notImplemented(); },
+        [this] /*0x80*/ { add(B_); },
+        [this] /*0x81*/ { add(C_); },
+        [this] /*0x82*/ { add(D_); },
+        [this] /*0x83*/ { add(E_); },
+        [this] /*0x84*/ { add(H_); },
+        [this] /*0x85*/ { add(L_); },
+        [this] /*0x86*/ { add(fetch8(HL_)); },
+        [this] /*0x87*/ { add(A_); },
+        [this] /*0x88*/ { adc(B_); },
+        [this] /*0x89*/ { adc(C_); },
+        [this] /*0x8A*/ { adc(D_); },
+        [this] /*0x8B*/ { adc(E_); },
+        [this] /*0x8C*/ { adc(H_); },
+        [this] /*0x8D*/ { adc(L_); },
+        [this] /*0x8E*/ { adc(fetch8(HL_)); },
+        [this] /*0x8F*/ { adc(A_); },
+        [this] /*0x90*/ { sub(B_); },
+        [this] /*0x91*/ { sub(C_); },
+        [this] /*0x92*/ { sub(D_); },
+        [this] /*0x93*/ { sub(E_); },
+        [this] /*0x94*/ { sub(H_); },
+        [this] /*0x95*/ { sub(L_); },
+        [this] /*0x96*/ { sub(fetch8(HL_)); },
+        [this] /*0x97*/ { sub(A_); },
+        [this] /*0x98*/ { sbc(B_); },
+        [this] /*0x99*/ { sbc(C_); },
+        [this] /*0x9A*/ { sbc(D_); },
+        [this] /*0x9B*/ { sbc(E_); },
+        [this] /*0x9C*/ { sbc(H_); },
+        [this] /*0x9D*/ { sbc(L_); },
+        [this] /*0x9E*/ { sbc(fetch8(HL_)); },
+        [this] /*0x9F*/ { sbc(A_); },
+        [this] /*0xA0*/ { andr(B_); },
+        [this] /*0xA1*/ { andr(C_); },
+        [this] /*0xA2*/ { andr(D_); },
+        [this] /*0xA3*/ { andr(E_); },
+        [this] /*0xA4*/ { andr(H_); },
+        [this] /*0xA5*/ { andr(L_); },
+        [this] /*0xA6*/ { andr(fetch8(HL_)); },
+        [this] /*0xA7*/ { andr(A_); },
+        [this] /*0xA8*/ { xorr(B_); },
+        [this] /*0xA9*/ { xorr(C_); },
+        [this] /*0xAA*/ { xorr(D_); },
+        [this] /*0xAB*/ { xorr(E_); },
+        [this] /*0xAC*/ { xorr(H_); },
+        [this] /*0xAD*/ { xorr(L_); },
+        [this] /*0xAE*/ { xorr(fetch8(PC_+1)); },
+        [this] /*0xAF*/ { xorr(A_); },
+        [this] /*0xB0*/ { orr(B_); },
+        [this] /*0xB1*/ { orr(C_); },
+        [this] /*0xB2*/ { orr(D_); },
+        [this] /*0xB3*/ { orr(E_); },
+        [this] /*0xB4*/ { orr(H_); },
+        [this] /*0xB5*/ { orr(L_); },
+        [this] /*0xB6*/ { orr(fetch8(PC_+1)); },
+        [this] /*0xB7*/ { orr(A_); },
+        [this] /*0xB8*/ { cp(B_); },
+        [this] /*0xB9*/ { cp(C_); },
+        [this] /*0xBA*/ { cp(D_); },
+        [this] /*0xBB*/ { cp(E_); },
+        [this] /*0xBC*/ { cp(H_); },
+        [this] /*0xBD*/ { cp(L_); },
+        [this] /*0xBE*/ { cp(fetch8(PC_+1)); },
+        [this] /*0xBF*/ { cp(A_); },
         [this] /*0xC0*/ { ret(!FlagZ_); },
         [this] /*0xC1*/ { pop(BC_); },
         [this] /*0xC2*/ { jp(!FlagZ_, fetch16(PC_+1)); },
         [this] /*0xC3*/ { jp(true, fetch16(PC_+1)); },
         [this] /*0xC4*/ { call(!FlagZ_, fetch16(PC_+1)); },
         [this] /*0xC5*/ { push(BC_); },
-        [this] /*0xC6*/ { notImplemented(); },
+        [this] /*0xC6*/ { add(fetch8(PC_+1)); },
         [this] /*0xC7*/ { rst(0x00); },
         [this] /*0xC8*/ { ret(FlagZ_); },
         [this] /*0xC9*/ { ret(true); },
         [this] /*0xCA*/ { jp(FlagZ_, fetch16(PC_+1)); },
-        [this] /*0xCB*/ { notImplemented(); },
+        [this] /*0xCB*/ { isPrefixed_ = true; },
         [this] /*0xCC*/ { call(FlagZ_, fetch16(PC_+1)); },
         [this] /*0xCD*/ { call(true, fetch16(PC_+1)); },
-        [this] /*0xCE*/ { notImplemented(); },
+        [this] /*0xCE*/ { adc(fetch8(PC_+1)); },
         [this] /*0xCF*/ { rst(0x08); },
         [this] /*0xD0*/ { ret(!FlagC_); },
         [this] /*0xD1*/ { pop(DE_); },
-        [this] /*0xD2*/ { notImplemented(); },
+        [this] /*0xD2*/ { jp(!FlagC_, fetch16(PC_+1)); },
         [this] /*0xD3*/ { notImplemented(); },
-        [this] /*0xD4*/ { jp(!FlagC_, fetch16(PC_+1)); },
+        [this] /*0xD4*/ { call(!FlagC_, fetch16(PC_+1)); },
         [this] /*0xD5*/ { push(DE_); },
-        [this] /*0xD6*/ { notImplemented(); },
+        [this] /*0xD6*/ { sub(fetch8(PC_+1)); },
         [this] /*0xD7*/ { rst(0x10); },
         [this] /*0xD8*/ { ret(FlagC_); },
         [this] /*0xD9*/ { reti(); },
@@ -227,7 +224,7 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0xDB*/ { notImplemented(); },
         [this] /*0xDC*/ { call(FlagC_, fetch16(PC_+1)); },
         [this] /*0xDD*/ { notImplemented(); },
-        [this] /*0xDE*/ { notImplemented(); },
+        [this] /*0xDE*/ { sbc(fetch8(PC_+1)); },
         [this] /*0xDF*/ { rst(0x18); },
         [this] /*0xE0*/ { ldd(0xFF00 + fetch8(PC_+1), A_); },
         [this] /*0xE1*/ { pop(HL_); },
@@ -235,15 +232,15 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0xE3*/ { notImplemented(); },
         [this] /*0xE4*/ { notImplemented(); },
         [this] /*0xE5*/ { push(HL_); },
-        [this] /*0xE6*/ { notImplemented(); },
+        [this] /*0xE6*/ { andr(fetch8(PC_+1)); },
         [this] /*0xE7*/ { rst(0x20); },
-        [this] /*0xE8*/ { notImplemented(); },
+        [this] /*0xE8*/ { addSP(); },
         [this] /*0xE9*/ { jp(true, HL_); },
         [this] /*0xEA*/ { ldd(fetch16(PC_+1), A_); },
         [this] /*0xEB*/ { notImplemented(); },
         [this] /*0xEC*/ { notImplemented(); },
         [this] /*0xED*/ { notImplemented(); },
-        [this] /*0xEE*/ { notImplemented(); },
+        [this] /*0xEE*/ { xorr(fetch8(PC_+1)); },
         [this] /*0xEF*/ { rst(0x28); },
         [this] /*0xF0*/ { ld(A_, fetch8(0xFF00 + fetch8(PC_+1))); },
         [this] /*0xF1*/ { popAF(); },
@@ -251,98 +248,163 @@ ProcArray CPU::getUnprefProcArray() {
         [this] /*0xF3*/ { di(); },
         [this] /*0xF4*/ { notImplemented(); },
         [this] /*0xF5*/ { push(AF_); },
-        [this] /*0xF6*/ { notImplemented(); },
+        [this] /*0xF6*/ { orr(fetch8(PC_+1)); },
         [this] /*0xF7*/ { rst(0x30); },
-        [this] /*0xF8*/ { ldhl_sp(fetch8(PC_+1)); },
+        [this] /*0xF8*/ { ldHLSP(fetch8(PC_+1)); },
         [this] /*0xF9*/ { ld16(SP_, HL_); },
         [this] /*0xFA*/ { ld(A_, fetch8(fetch16(PC_+1))); },
         [this] /*0xFB*/ { ei(); },
         [this] /*0xFC*/ { notImplemented(); },
         [this] /*0xFD*/ { notImplemented(); },
-        [this] /*0xFE*/ { notImplemented(); },
+        [this] /*0xFE*/ { cp(fetch8(PC_+1)); },
         [this] /*0xFF*/ { rst(0x38); }
     };
 }
 
-/*################### misc ###################*/
-void CPU::ccf() { FlagC_.complement(); }
-void CPU::scf() { /* done by handleFlags */ }
-void CPU::nop() { /* do nothing */ }
-void CPU::halt() {
-    if(IME_)
-        isHalted_ = true;
-    else {
-        if(IE_ & IF_) {
-            isHaltBug_ = true;
-        }
-        else
-            isHalted_ = true;
-    }
-}
-void CPU::cpl() { A_=~A_; }
-void CPU::ei() { isEISet_ = true; }
-void CPU::di() { isDISet_ = true; }
-void CPU::daa() {
-    if(!FlagN_) {
-        if(FlagC_ || A_ > 0x99) {
-            A_ += 0x60;
-            FlagC_.set(true);
-        }
-        if(FlagH_ || (A_ & 0x0F) > 0x09)
-            A_ += 0x6;
-    }
-    else {
-        if(FlagC_)
-            A_ -= 0x60;
-        if(FlagH_)
-            A_ -=0x6;
-    }
-    checkFlagZ();
-}
-void CPU::stop() { isStopped_ = true; }
+/*################### 8-bit arithmetic and logic ###################*/
 
-/*################### LOAD ###################*/
-void CPU::ld(r8& l, const r8 r) { l = r; }
-void CPU::ld16(r16& l, const u16 r) { l = r; }
-void CPU::ldd(u16 const addr, u8 const byte) { memory_.write(byte, addr); }
-void CPU::ldd16(u16 const addr, const r16 word) { 
-    memory_.write(word.lo_, addr);
-    memory_.write(word.hi_, addr+1);
-}
-
-/*################### 8bit arithmetic ###################*/
 void CPU::inc(r8& r) {
     FlagZ_.set(r+1 > 0xFF);
     FlagH_.set(Flag::checkH(r, 1, r+1));
     ++r;
 }
-void CPU::inc(r16& r) { ++r; }
+
 void CPU::incd(const u16 addr) {
     u8 t = fetch8(addr);
     inc(t);
     memory_.write(t, addr);
 }
+
 void CPU::dec(r8& r) {
     FlagZ_.set(r-1 == 0);
     FlagH_.set(Flag::checkH(r, 1, r-1));
     --r;
 }
-void CPU::dec(r16& r) { --r; }
+
 void CPU::decd(const u16 addr) {
     u8 b = fetch8(addr);
     dec(b);
     memory_.write(b, addr);
 }
 
-/*################### JUMP and SUBROUTINES ###################*/
-
-void CPU::rst(const u8 n) {
-    memory_.write(PC_.hi_, --SP_);
-    memory_.write(PC_.lo_, --SP_);
-    PC_.hi_ = 0;
-    PC_.lo_ = n;
-    incrementPC_ = false;
+void CPU::addHelper(const u8 r, const bool addFlagC) {
+    r16 res = A_ + r;
+    if(addFlagC)
+        res += static_cast<u16>(FlagC_);
+    FlagZ_.set(res);
+    FlagH_.set(Flag::checkH(A_, r, res.lo_));
+    FlagC_.set(res > 0x00FF);
+    A_ = res;
 }
+
+void CPU::add(const u8 r) {
+    addHelper(r, false);
+}
+
+void CPU::adc(const u8 r) {
+    addHelper(r, true);
+}
+
+void CPU::subHelper(const u8 r, const bool subFlagC) {
+    r16 res = A_ - r;
+    if(subFlagC)
+        res -= static_cast<u16>(FlagC_);
+    FlagZ_.set(res);
+    FlagH_.set(Flag::checkH(A_, r, res.lo_));
+    A_ = res;
+}
+
+// TODO: Add tests
+void CPU::sub(const u8 r) {
+    subHelper(r, false);
+}
+
+// TODO: Add tests
+void CPU::sbc(const u8 r) {
+    subHelper(r, false);
+}
+
+// TODO: Add tests
+void CPU::andr(const u8 r) {
+    A_ &= r;
+    FlagZ_.set(A_);
+}
+
+// TODO: Add tests
+void CPU::xorr(const u8 r) {
+    A_ ^= r;
+    FlagZ_.set(A_);
+}
+
+// TODO: Add tests
+void CPU::orr(const u8 r) {
+    A_ |= r;
+    FlagZ_.set(A_);
+}
+
+// TODO: Add tests
+void CPU::cp(const u8 r) {
+    u8 res = A_ - r;
+    FlagZ_.set(res);
+    FlagH_.set(Flag::checkH(A_, r, res));
+    FlagC_.set(res < 0);
+}
+
+/*################### 16-bit arithmetic and logic ###################*/
+
+// TODO: Add tests
+void CPU::add16(const r16 rp) {
+    FlagH_.set(Flag::checkH16(HL_, rp, HL_ + rp));
+    FlagC_.set(HL_ + rp > 0xFFFF);
+    HL_ += rp;
+}
+
+void CPU::dec(r16& r) {
+    --r;
+}
+
+void CPU::inc(r16& r) {
+    ++r;
+}
+
+/*################### bit shift ###################*/
+
+void CPU::rla() {
+    rl(A_);
+}
+
+void CPU::rlca() {
+    rlc(A_);
+}
+
+void CPU::rra() {
+    rr(A_);
+}
+
+void CPU::rrca() {
+    rrc(A_);
+}
+
+/*################### LOAD ###################*/
+
+void CPU::ld(r8& l, const r8 r) {
+    l = r;
+}
+
+void CPU::ld16(r16& l, const u16 r) {
+    l = r;
+}
+
+void CPU::ldd(u16 const addr, u8 const byte) {
+    memory_.write(byte, addr);
+}
+
+void CPU::ldd16(u16 const addr, const r16 word) { 
+    memory_.write(word.lo_, addr);
+    memory_.write(word.hi_, addr+1);
+}
+
+/*################### JUMP and SUBROUTINES ###################*/
 
 void CPU::jp(const bool cond, const u16 addr) {
     if(cond) {
@@ -367,6 +429,14 @@ void CPU::call(const bool cond, const u16 addr) {
         isCondMet_ = true;
         incrementPC_ = false;
     }
+}
+
+void CPU::rst(const u8 n) {
+    memory_.write(PC_.hi_, --SP_);
+    memory_.write(PC_.lo_, --SP_);
+    PC_.hi_ = 0;
+    PC_.lo_ = n;
+    incrementPC_ = false;
 }
 
 void CPU::ret(const bool cond) {
@@ -403,7 +473,7 @@ void CPU::push(const r16 rp) {
     memory_.write(rp.lo_, --SP_);
 }
 
-void CPU::ldhl_sp(const int8_t d) {
+void CPU::ldHLSP(const int8_t d) {
     u16 res = SP_ + static_cast<u16>(d);
     if(d >= 0) {
         FlagC_.set((SP_ & 0xFF) + d > 0xFF);
@@ -416,49 +486,67 @@ void CPU::ldhl_sp(const int8_t d) {
     HL_ = res;
 }
 
-/*################### STACK ###################*/
-
-void CPU::rlc(u8& r) {
-    bool msb = Utils::getBit(r, 7);
-    r <<= 1;
-    Utils::setBit(r, 0, msb);
-    FlagC_.set(msb);
-    FlagZ_.set(r == 0);
+void CPU::addSP() {
+    int8_t val = fetch8(PC_+1);
+    FlagH_.set(Flag::checkH(SP_, val, SP_ + val));
+    FlagC_.set(SP_ + val > 0xFFFF);
+    SP_ += val;
 }
 
-void CPU::rlca() { rlc(A_); }
+/*################### misc ###################*/
 
-void CPU::rrc(r8& r) {
-    bool lsb = Utils::getBit(r, 0);
-    r >>= 1;
-    Utils::setBit(r, 7, lsb);
-    FlagC_.set(lsb);
-    FlagZ_.set(r == 0);
+void CPU::nop() {
+    /* do nothing */
 }
 
-void CPU::rrca() { rrc(A_); }
-
-void CPU::rl(r8& r) {
-    bool msb = Utils::getBit(r, 7);
-    r <<= 1;
-    Utils::setBit(r, 0, FlagC_);
-    FlagC_.set(msb);
-    FlagZ_.set(r == 0);
+void CPU::scf() {
+    /* done by handleFlags */
 }
 
-void CPU::rla() { rl(A_); }
-
-void CPU::rr(r8& r) {
-    bool lsb = Utils::getBit(r, 0);
-    r >>= 1;
-    Utils::setBit(r, 7, FlagC_);
-    FlagC_.set(lsb);
-    FlagZ_.set(r == 0);
+void CPU::ccf() {
+    FlagC_.complement();
 }
 
-void CPU::rra() { rr(A_); }
+void CPU::cpl() {
+    A_ = ~A_;
+}
 
+void CPU::ei() {
+    isEISet_ = true;
+}
 
+void CPU::di() {
+    isDISet_ = true;
+}
 
+void CPU::stop() { isStopped_ = true; }
 
+void CPU::halt() {
+    if(IME_)
+        isHalted_ = true;
+    else {
+        if(IE_ & IF_) {
+            isHaltBug_ = true;
+        }
+        else
+            isHalted_ = true;
+    }
+}
 
+void CPU::daa() {
+    if(!FlagN_) {
+        if(FlagC_ || A_ > 0x99) {
+            A_ += 0x60;
+            FlagC_.set(true);
+        }
+        if(FlagH_ || (A_ & 0x0F) > 0x09)
+            A_ += 0x6;
+    }
+    else {
+        if(FlagC_)
+            A_ -= 0x60;
+        if(FlagH_)
+            A_ -=0x6;
+    }
+    FlagZ_.set(A_);
+}
