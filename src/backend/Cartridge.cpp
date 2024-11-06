94417c9fea29bf48458cc692de1732358646ae26
KiwiGrenade <kubajaskow@gmail.com>
2024-11-06 22:26:20 +0100

    Add constructor for Cartridge and test it

--- src/backend/Cartridge.cpp
+++ src/backend/Cartridge.cpp
@@ -1,27 +1,33 @@
 #include "Cartridge.hpp"
+#include <array>
+#include <filesystem>
+#include <fstream>
 #include "utils.hpp"
 
-Cartridge::Cartridge(const QByteArray& arr)
-    : title_(extractTitle(arr))
-    , rom_(extractROM(arr)){
+Cartridge::Cartridge(const std::string& filePath) 
+    : rom_(extractROM(filePath))
+    , title_(extractTitle()) {
 }
 
-std::array<u8, Cartridge::romSize_> Cartridge::extractROM(const QByteArray& arr) {
-    if(arr.size() < romSize_)
-        Utils::error("Array too small to initialize cartridge ROM");
+std::array<u8, Cartridge::romSize_> Cartridge::extractROM(const std::string& filePathStr) {
+    const std::filesystem::path filePath { filePathStr };
+    if(not std::filesystem::exists(filePath))
+        Utils::error("Filename not provided for cartridge!");
+
     std::array<u8, Cartridge::romSize_> rom;
-    for(u16 i = 0; i < romSize_; ++i) {
-        rom[i] = arr[i];
+    
+    std::ifstream file { filePath, std::ios::binary };
+    if(file) {
+        file.read(reinterpret_cast<char*>(rom.data()), static_cast<long>(romSize_));
+        if(file.eof())
+            Utils::warning("Reached end of file!");
     }
+    file.close();
+
     return rom;
 }
 
-std::string Cartridge::extractTitle(const QByteArray& arr) {
-    if(arr.size() < 0x134+15)
-        Utils::error("Failed to extract cartridge title: array too small");
-    std::string title = "";
-    for(u8 i = 0; i < 15; ++i) {
-        title += arr[0x134+i];
-    }
-    return title;
+std::string Cartridge::extractTitle() {
+    auto titleStart = rom_.begin() + 0x0134;
+    return std::string(titleStart, titleStart + 15);
 }
--- src/backend/Cartridge.hpp
+++ src/backend/Cartridge.hpp
@@ -3,19 +3,20 @@
 #include "utils.hpp"
 
 #include <QByteArray>
+#include <filesystem>
 
 class Cartridge {
 public:
     Cartridge() = default;
-    Cartridge(const QByteArray& arr);
+    Cartridge(const std::string& filePath);
     
     inline u8 read(const u16 addr) { return rom_[addr]; };
     inline std::string getTitle() { return title_; };
 
     static constexpr u16 romSize_ = 0x8000;
 private:
-    std::array<u8, romSize_> extractROM(const QByteArray& arr);
-    std::string extractTitle(const QByteArray& arr);
-    std::string title_;
+    std::array<u8, romSize_> extractROM(const std::string& filePathStr);
+    std::string extractTitle();
     std::array<u8, romSize_> rom_;
+    std::string title_;
 };
