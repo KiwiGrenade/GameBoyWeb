e158728808d4162139a799ee5233d94356c83ffa
KiwiGrenade <kubajaskow@gmail.com>
2024-11-07 13:03:30 +0100

    Add base for integration tests

---
+++ tests/integration/CPUIntegrationTest.cpp
@@ -0,0 +1,8 @@
+#include "GameBoy.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("01-special.gb", "[tag1], [tag2]") {
+    Cartridge car {"/home/jaskow/Repo/GameBoyWeb/roms/tests/"};
+
+}
