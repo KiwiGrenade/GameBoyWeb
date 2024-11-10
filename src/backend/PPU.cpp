93b8481607217217d0fe60194aafad2a27659b0d
KiwiGrenade <kubajaskow@gmail.com>
2024-11-10 22:42:24 +0100

    Add definition of PPU::setLCDStatus()

--- src/backend/PPU.cpp
+++ src/backend/PPU.cpp
@@ -1,20 +1,79 @@
 #include "PPU.hpp"
 #include "InterruptController.hpp"
+#include "utils.hpp"
 
 PPU::PPU(InterruptController& ic) 
     : ic_(ic) {
 }
 
 void PPU::reset() {
-
+    LY_ = 0;
+    scanlineCounter_ = 0;
 }
 
 void PPU::setLCDStatus() {
+    if(not isLCDEnabled()) {
+        scanlineCounter_ = 0;
+        LY_ = 0;
+        STAT_ &= 0b11111100;
+        Utils::setBit(STAT_, 0, false);
+        return;
+    }
 
-}
+    u8 currMode = STAT_ & 3;
+    u8 status = STAT_;
 
-bool PPU::isLCDEnabled() {
-    return false;
+    u8 mode = 0;
+    bool requestInterrupt = false;
+
+    // VBlank - set mode to 1 
+    if(LY_ >= 144) {
+        mode = 1;
+        Utils::setBit(status, 0, true); 
+        Utils::setBit(status, 1, false);
+        requestInterrupt = Utils::getBit(status, 4);
+    }
+    else {
+        constexpr int mode2bounds = 80;
+        constexpr int mode3bounds = mode2bounds + 172;
+
+        // mode 2
+        if(scanlineCounter_ <= mode2bounds) {
+            mode = 2;
+            Utils::setBit(status, 0, false);
+            Utils::setBit(status, 1, true);
+            requestInterrupt = Utils::getBit(status, 5);
+        }
+        // mode 3
+        else if(scanlineCounter_ <= mode3bounds) {
+            mode = 3;
+            Utils::setBit(status, 0, true);
+            Utils::setBit(status, 1, true);
+        }
+        // mode 0
+        else {
+            mode = 0;
+            Utils::setBit(status, 0, false);
+            Utils::setBit(status, 1, false);
+            requestInterrupt = Utils::getBit(status, 3);
+        }
+    }
+    
+    // request an interrupt when entering new mode
+    if(requestInterrupt && (currMode != mode)) {
+        ic_.requestInterrupt(InterruptController::Type::LCD);
+    }
+
+    if(LY_ == LYC_) {
+        Utils::setBit(status, 2, true);
+        if(Utils::getBit(status, 6))
+            ic_.requestInterrupt(InterruptController::Type::LCD);
+    }
+    else {
+        Utils::setBit(status, 2, 0);
+    }
+
+    STAT_ = status;
 }
 
 void PPU::drawScanline() {
--- src/backend/PPU.hpp
+++ src/backend/PPU.hpp
@@ -8,14 +8,19 @@ public:
     PPU(InterruptController& ic);
     void update(u16 cycles);
     void reset();
+    inline u8 getLY() { return LY_; };
+    inline void setLY(u8 b) { LY_ = b; };
 
 protected:
     void setLCDStatus();
-    bool isLCDEnabled();
+    inline bool isLCDEnabled() { return Utils::getBit(LCDC_, 7); };
     void drawScanline();
 
     InterruptController& ic_;
 
     u16 scanlineCounter_;
-    u8 LY_; // 0xFF44
+    u8 LCDC_;   // 0xFF40
+    u8 STAT_;   // 0xFF41
+    u8 LY_;     // 0xFF44
+    u8 LYC_;    // 0xFF45
 };
