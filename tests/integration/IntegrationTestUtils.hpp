6021d7f1b0c62409442261b2ab2348782eafe3ed
KiwiGrenade <kubajaskow@gmail.com>
2024-11-12 22:12:26 +0100

    Bug fixed

---
+++ tests/integration/IntegrationTestUtils.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+namespace TestUtils 
+{
+namespace TestNames 
+{
+namespace CpuInstrs 
+{
+    std::string main { "cpu_instrs" };
+    std::array<std::string, 11> individual {
+        "01-special",
+        "02-interrupts",
+        "03-op sp,hl",
+        "04-op r,imm",
+        "05-op rp",
+        "06-ld r,r",
+        "07-jr,jp,call,ret,rst",
+        "08-misc instrs",
+        "09-op r,r",
+        "10-bit ops",
+        "11-op a,(hl)"
+    };
+} // CpuInstrs
+} // TestNames
+} // TestUtils
