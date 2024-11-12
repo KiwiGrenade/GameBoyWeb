0b2ae1d742d4949a093d4b31b0860178e098f5c9
KiwiGrenade <kubajaskow@gmail.com>
2024-11-13 00:00:08 +0100

    Change GameBoy

--- src/backend/GameBoy.hpp
+++ src/backend/GameBoy.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <condition_variable>
+#include <cstdint>
 #include <memory>
 
 #include <QByteArray>
 #include <QThread>
+#include <thread>
 
 #include "Cartridge.hpp"
 #include "InterruptController.hpp"
@@ -14,26 +17,42 @@
 #include "Memory.hpp"
 #include "CPU.hpp"
 
-class GameBoy : public QThread {
-    Q_OBJECT
+class GameBoy {
 public:
     GameBoy();
     ~GameBoy();
     
-    void loadCartridge(const std::shared_ptr<Cartridge> cartridge) { memory_.loadCartridge(cartridge); }
-    void reset();
+    void runConcurrently();
+    void setRenderer(Renderer *r);
+    void execute(uint32_t cycles);
+
     void stop();
     void pause();
-    void emulateStep();
+    void resume();
+    void reset();
+    uint64_t step(uint64_t cycles);
+
+    void press(Joypad::Button button);
+    void release(Joypad::Button button);
+    bool isRunning() const;
+
+    void loadCartridge(const std::shared_ptr<Cartridge> cartridge);
     std::string getCPUDebugDump();
     std::string getSerialOutput();
-    void run() override; // start
     uint64_t update(uint32_t nCycles);
 
 protected:
+    void run();
+    std::string romTitle_ {};
+    bool isRomLoaded_ = false;
+
+    std::thread thread_;
+    
+    std::condition_variable pause_cv_;
+    std::atomic<bool> isStopped_ = false;
+    mutable std::mutex mutex_;
     // flags
     bool isPaused_ = false;
-    bool isStopped_ = false;
     InterruptController ic_;
     Timer timer_;
     Joypad joypad_;
